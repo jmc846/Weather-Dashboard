cd9f313b73e67e4236b5bfde8afb6a9e0e330b98
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-07-13 23:02:06 -0400

    updates to js and json

--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
 var APIkey = "e0d9c528e3291fdb870ce8252b3e562b";
 var queryUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&%20exclude=hourly,daily&appid=e0d9c528e3291fdb870ce8252b3e562b" +
   "q=USA&appid=" + APIkey;
-var button= document.querySelector('button')
-var userInput=document.querySelector('.userInput')
+var button = document.querySelector('button')
+var userInput = document.querySelector('.userInput')
 
 var card = $("<div>").addClass("card");
 var cardBody = $("<div>").addClass("cardbody");
@@ -19,118 +19,228 @@ cardBody.append("#temperature");
 card.append(cardBody);
 $("#current").append(card);
 
-document.getElementById("submitButton").addEventListener('click', function(){
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&%20exclude=hourly,daily&='  +userInput.value+
-'&appid=e0d9c528e3291fdb870ce8252b3e562b')
-.then(response => response.json())
-.then(data =>{ console.log(data)
-})
+// api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=$(appId)&
 
-.catch(err => alert("Wrong city name"))
+document.getElementById("getWeather").addEventListener('click', function () {
+  getWeather(userInput.value, 'https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&%20exclude=hourly,daily&=' + userInput.value +
+    '&appid=e0d9c528e3291fdb870ce8252b3e562b')
+
 })
+// .then(response => { 
+
+//   console.log(response)
+// // .then(data =>{ console.log(data)
+
+//   // Transfer content to HTML
+//   $("#city").text("<h1>" + response.name + " Weather Details</h1>");
+//   $("#date").text("Date" + response.date);
+//   $("#temp-2").text("temperature" + response.temperature[0]);
+//   $("#wind").text("Wind Speed: " + response.wind.speed[]);
+//   $("#humidity").text("Humidity: " + response.main.humidity[0]);
+//   $("#uvIndex").text("Uv Index" + response.uv.index[0]);
+
+//   $("div").html(`#city${response.name}weather Deatails`)
+
+// return true
+// // })
+
+// .catch(err => alert("Wrong city name"))
+// })
 // GIVEN a weather dashboard with form inputs
-function getWeather(userInput) {
+function getWeather(userInput, queryUrl) {
   $.ajax({
     url: queryUrl,
     method: "GET",
     dataType: "json",
-    success: function (data) {
-      if (inputStorage.indexOf(userInput) === -1){
-        inputStorage.push(userInput)
-      window.localStorage.setItem("inputStorage", JSON.stringify(inputStorage)),
 
-      searchHistory(userInput)
-    }}
+    success: function (data) {
+      console.log(data)
+      // if (inputStorage.indexOf(userInput) === -1){
+      //   inputStorage.push(userInput)
+      // window.localStorage.setItem("inputStorage", JSON.stringify(data)),
+
+      // searchHistory(userInput)
+      // 
+    }
     // // WHEN I search for a city-Event listener need for search city button 
   });
 
-    $("#citysearch").on("click", function () {
-      getWeather($(this).text());
-      console.log(this)
+  $("#citysearch").on("click", function () {
+    getWeather($(this).text());
+    console.log(this)
 
-      function searchHistory(text) {
-        var li = $("<li>").addClass("search-list").text(text);
-        $("#searchhistory").append(li)
+    function searchHistory(text) {
+      var li = $("<li>").addClass("search-list").text(text);
+      $("#day-1").append(li)
 
-      }
+    }
 
-    
-      // This .on("click") function will trigger the AJAX Call 
-      $(document).ready(function () {
-        $("searchcity").on("click", function (event) {
-          event.preventDefault();
-          var userInput = $("#find-city").val();
-          console.log(event.target);
-          $("city-input").val("");
-          getWeather(userInput)
-        })
-      });
 
-      // // THEN I am presented with current and future conditions for that city and that city is added to the search history
-      $("#currentforecast").text("<Current forecast>");
-        $("#futureforecast").text("<future forecast>");
-      // // WHEN I view current weather conditions for that city
-      function displayCityWeather() {
+    // This .on("click") function will trigger the AJAX Call 
+    $(document).ready(function () {
+      $("#citysearch").on("click", function (event) {
+        event.preventDefault();
+        var userInput = $("#find-city").val();
+        console.log(event.target);
+        $("city-input").val("");
+        getWeather(data)
+      })
+    });
+
+    // // THEN I am presented with current and future conditions for that city and that city is added to the search history
+    $("#currentforecast").text("<Current forecast>");
+    $("#futureforecast").text("<future forecast>");
+    // // WHEN I view current weather conditions for that city
+
+
+    // var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city
+    function displayCityWeather() {
+      $.ajax({
+        url: queryUrl,
+        method: "GET"
+      }).then(function (response) {
+        console.log(response);
         var city = $(this).attr("data-name")
         console.log('city:' + city)
-        var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city
+        var queryUrl = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon;
+        var uvIndex = uvUrl.value
 
         $.ajax({
-          url: queryUrl,
+          url: uvUrl,
           method: "GET"
         }).then(function (response) {
-          $("#find-city").text(JSON.stringify(response));
-        }),
-          console.log(this);
-        console.log(event.target);
-      };
-      // // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-      // Transfer content to HTML
-      $(".city").text("<h1>" + response.name + " Weather Details</h1>");
-      $(".date").text("Date" + response.date);
-      $(".wind").text("Wind Speed: " + response.wind.speed);
-      $(".humidity").text("Humidity: " + response.main.humidity);
-      $(".uv").text("Uv Index" + response.uv.index);
+        })
+        console.log(uvIndex)
+          $("#current").text(JSON.stringify(response));
+        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
-      // Convert the temp to fahrenheit
-      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        // add temp content to html
 
-      // add temp content to html
-      $(".temp").text("Temperature (K) " + response.main.temp);
-      $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+        $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".uv").text("UV Index: " + uvIndex);
+      });
 
-      // Log the data in the console as well
-      console.log("Wind Speed: " + response.wind.speed);
-      console.log("Humidity: " + response.main.humidity);
-      console.log("Temperature (F): " + tempF);
+      $.ajax({
+        url: queryUrl,
+        method: "GET"
+      }).then(function (response) {
+        $("#find-city").text(JSON.stringify(response));
+      }),
+        console.log(this);
+      console.log(event.target);
+    };
+    //  THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+    //  Transfer content to HTML
+    $(".city").text("<h1>" + response.name + " Weather Details</h1>");
+    $(".date").text("Date" + response.date);
+    $(".wind").text("Wind Speed: " + response.wind.speed);
+    $(".humidity").text("Humidity: " + response.main.humidity);
+    $(".uv").text("Uv Index" + response.uvIndex);
 
-      // // WHEN I view the UV index
-      // // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-      // // WHEN I view future weather conditions for that city
-      // // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-      // // WHEN I click on a city in the search history
-      // // THEN I am again presented with current and future conditions for that city
-      // // WHEN I open the weather dashboard
-      // // THEN I am presented with the last searched city forecast
-    })
+    // Convert the temp to fahrenheit
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
-      // Function for displaying weather data
-      function renderButtons() {
+    // add temp content to html
+    $(".temp").text("Temperature (K) " + response.main.temp);
+    $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
 
-        // $('#buttons-view').html('');
-        $('#city-view').empty();
-        for (var i = 0; i < city.length; i++) {
-          console.log(movies[i]);
+    // Log the data in the console as well
+    console.log("Wind Speed: " + response.wind.speed);
+    console.log("Humidity: " + response.main.humidity);
+    console.log("Temperature (F): " + tempF);
+
+    // // WHEN I view the UV index
+    $(".uv").text("Uv Index" + response.uv.index);
+    // // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
+    // // WHEN I view future weather conditions for that city
+    $("#futureforecast").text("<future forecast>");
+
+    // // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+
+    $(".date").text("Date" + response.date);
+    $(".temperature").text("temperature"+response.temperature);
+    $(".wind").text("Wind Speed: " + response.wind.speed);
+    $(".humidity").text("Humidity: " + response.main.humidity);
+    $(".uv").text("Uv Index" + response.uv.index);
+
+    $(".date-2").text("Date" + response.date);
+    $(".day-2temperature").text("temperature"+response.temperature);
+    $(".day-2windSpeed").text("Wind Speed: " + response.wind.speed);
+    $(".day-2Humidity").text("Humidity: " + response.main.humidity);
+    $(".day-2uvIndex").text("Uv Index" + response.uv.index);
 
 
+    $(".date-3").text("Date" + response.date);
+    $(".day-3temperature").text("temperature"+response.temperature);
+    $(".day-3windSpeed").text("Wind Speed: " + response.wind.speed);
+    $(".day-3Humidity").text("Humidity: " + response.main.humidity);
+    $(".day-3uvIndex").text("Uv Index" + response.uv.index);
 
-          // create that button
-          var button = $("<button>");
-          // we need the attribute data-name
-          button.attr("data-name", city[i]);
-          // we need the name on the button
-          button.text(city[i]);
-        };
-      };
     
-  }
+    $(".date-4").text("Date" + response.date);
+    $(".day-4temperature").text("temperature"+response.temperature);
+    $(".day-4windSpeed").text("Wind Speed: " + response.wind.speed);
+    $(".day-4Humidity").text("Humidity: " + response.main.humidity);
+    $(".day-4uvIndex").text("Uv Index" + response.uv.index);
+
+    
+    $(".date-5").text("Date" + response.date);
+    $(".day-5temperature").text("temperature"+response.temperature);
+    $(".day-5windSpeed").text("Wind Speed: " + response.wind.speed);
+    $(".day-5Humidity").text("Humidity: " + response.main.humidity);
+    $(".day-5uvIndex").text("Uv Index" + response.uv.index);
+
+
+
+
+
+
+    // // WHEN I click on a city in the search history
+
+    // // THEN I am again presented with current and future conditions for that city
+
+    // // WHEN I open the weather dashboard
+
+    // // THEN I am presented with the last searched city forecast
+  })
+
+   // Function for displaying movie data
+   function renderButtons() {
+    //load persistent data if it isn't null
+if(localStorage.getItem("city")){
+      movies = JSON.parse(localStorage.getItem("city"));
+// Deletes the city prior to adding new city
+        // (this is necessary otherwise you will have repeat buttons)
+        $("#buttons-view").empty();
+  /// Loops through the array of city
+for (var i = 0; i < city.length; i++) {
+
+  // Then dynamicaly generates buttons for each movie in the array
+  // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+  var a = $("<button>");
+  // Adds a class of movie to our button
+  a.addClass("city");
+  // Added a data-attribute
+  a.attr("data-name", city[i]);
+  // Provided the initial button text
+  a.text(city[i]);
+  // Added the button to the buttons-view div
+  $("#buttons-view").append(a);
+
+
+  var a = $("<button>");
+  // Adds a class of movie to our button
+  a.addClass("temperature");
+  // Added a data-attribute
+  a.attr("data-name", temperature[i]);
+  // Provided the initial button text
+  a.text(temperature[i]);
+  // Added the button to the buttons-view div
+  $("#buttons-view").append(a);
+}
+}
+}}
